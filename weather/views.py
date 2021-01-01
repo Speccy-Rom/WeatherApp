509c9eb570eb7559caae0cb6ed49f42333b1d7bc
Speccy-rom <speccy.rom@gmail.com>
2021-01-01 23:59:56 +0300

    создал модели городов

--- weather/views.py
+++ weather/views.py
@@ -1,19 +1,24 @@
 import requests
 from django.shortcuts import render
+from .models import City
 
 
 def index(request):
     appid = "3c77424120dc53262fe18cba74eb9d70"
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=' + appid
     city = 'London'
-    res = requests.get(url.format(city)).json()
 
-    city_info = {
-        'city': city,
-        'temp': res['main']['temp'],
-        'icon': res['weather'][0]['icon']
-    }
+    cities = City.objects.all()
+    all_cities = []
+    for city in cities:
+        res = requests.get(url.format(city.name)).json()
+        city_info = {
+            'city': city.name,
+            'temp': res['main']['temp'],
+            'icon': res['weather'][0]['icon']
+        }
+        all_cities.append(city_info)
+    context = {'all_info': all_cities}
 
-    context = {'info': city_info}
 
     return render(request, 'weather/index.html', context)
