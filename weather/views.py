50a1939ef5166f9beaa10ceb97fde7dea06a0756
Speccy-rom <speccy.rom@gmail.com>
2021-01-01 22:03:45 +0300

    install api openweathermap

--- weather/views.py
+++ weather/views.py
@@ -1,2 +1,11 @@
+import requests
 from django.shortcuts import render
 
+
+def index(request):
+
+    appid = "3c77424120dc53262fe18cba74eb9d70"
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid=' + appid
+    city = 'London'
+    res = requests.get(url.format(city))
+    return render(request, 'weather/index.html')
