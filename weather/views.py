ef52e2aa2a453a2cb5dfbb3e6da41b4a6328de7d
Speccy-rom <speccy.rom@gmail.com>
2021-01-01 23:27:48 +0300

    views вывод одного города с temp.

--- weather/views.py
+++ weather/views.py
@@ -3,9 +3,17 @@ from django.shortcuts import render
 
 
 def index(request):
-
     appid = "3c77424120dc53262fe18cba74eb9d70"
-    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid=' + appid
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=' + appid
     city = 'London'
-    res = requests.get(url.format(city))
-    return render(request, 'weather/index.html')
+    res = requests.get(url.format(city)).json()
+
+    city_info = {
+        'city': city,
+        'temp': res['main']['temp'],
+        'icon': res['weather'][0]['icon']
+    }
+
+    context = {'info': city_info}
+
+    return render(request, 'weather/index.html', context)
